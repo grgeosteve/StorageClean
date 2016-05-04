3d6c35fe3d132af7abc87487fd6af561bc895076
Stavros Georgousis <grgeosteve@gmail.com>
2016-05-05 01:11:01 +0300

    Initial test for recursive file hash generator

---
+++ storageHash.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+import hashlib
+import sys
+import getopt
+import os
+
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def crawl(basedir, recursive):
+    for filename in os.listdir(basedir):
+        filename = basedir+filename
+        if os.path.isdir(filename):
+            if recursive:
+                crawl(filename + "/", recursive)
+            else:
+                continue
+        elif os.path.islink(filename):
+            continue
+        else:
+            filehash = md5(filename)
+            print("{0} {1}".format(filehash, filename))
+
+            
+def main(argv):
+    inputdir = ''
+    recursive = False
+    try:
+        opts, args = getopt.getopt(argv, "d:r", ["idir=", "recursive"])
+    except getopt.GetoptError:
+        print("dMd5sum.py -d <inputdir> [-r]")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-d", "--idir"):
+            inputdir = arg
+        elif opt in ("-r", "--recursive"):
+            recursive = True
+        else:
+            assert False, "unhandled option"
+
+    print(inputdir)
+    crawl(inputdir, recursive)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
