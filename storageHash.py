e5dd511aacb005fe9b35e9d287c3512a7471e54c
Stavros Georgousis <grgeosteve@gmail.com>
2016-05-06 17:19:51 +0300

    Change to object based crawler - Do some tests

--- storageHash.py
+++ storageHash.py
@@ -20,27 +20,62 @@ def isGrpReadable(filepath):
     return bool(st.st_mode & stat.S_IRGRP)
 
 
-def crawl(basedir, recursive):
-    for filename in os.listdir(basedir):
-        filename = basedir+filename
-        if os.path.isdir(filename):
-            if recursive and isGrpReadable(filename):
-                crawl(filename + "/", recursive)
+class Crawler:
+    """A class for filesystem crawling / computing md5 hash"""
+
+    def __init__(self, basedir, recursive=False, verbose=False):
+        self.verbose = verbose
+        self.recursive = recursive
+        self.basedir = basedir
+        self.depth = 0 # Depth in directory from original
+
+    def crawl(self):
+        for filename in os.listdir(self.basedir):
+            filename = self.basedir+filename
+            if os.path.isdir(filename):
+                if self.recursive and isGrpReadable(filename):
+                    subcrawler = Crawler(filename+"/", self.recursive,
+                                         self.verbose)
+                    subcrawler.execute()
+                else:
+                    continue
+            elif os.path.isfile(filename) and isGrpReadable(filename):
+                filehash = md5(filename)
+                lastmod = os.path.getmtime(filename)
+                if self.verbose:
+                    print("{0} {1} {2}".format(filehash, lastmod, filename))
             else:
                 continue
-        elif os.path.isfile(filename) and isGrpReadable(filename):
-            filehash = md5(filename)
-            lastmod = os.path.getmtime(filename)
-            print("{0} {1} {2}".format(filehash, lastmod, filename))
-        else:
-            continue
 
+    def testCrawl(self):
+        for filename in os.listdir(self.basedir):
+            filename = self.basedir+filename
+            if os.path.isdir(filename):
+                if self.recursive and isGrpReadable(filename):
+                    subcrawler = Crawler(filename+"/", self.recursive,
+                                         self.verbose)
+                    subcrawler.execute()
+                else:
+                    continue
+            elif os.path.isfile(filename) and isGrpReadable(filename):
+                filehash = md5(filename)
+                lastmod = os.path.getmtime(filename)
+                if self.verbose:
+                    print("{0} {1} {2}".format(filehash, lastmod, filename))
+            else:
+                continue
             
+    def execute(self):
+        self.testCrawl()
+
+
 def main(argv):
     inputdir = ''
+    verbose = False
     recursive = False
     try:
-        opts, args = getopt.getopt(argv, "d:r", ["idir=", "recursive"])
+        opts, args = getopt.getopt(argv, "d:rv",
+                                   ["idir=", "recursive", "verbose"])
     except getopt.GetoptError:
         print("dMd5sum.py -d <inputdir> [-r]")
         sys.exit(2)
@@ -49,12 +84,15 @@ def main(argv):
             inputdir = arg
         elif opt in ("-r", "--recursive"):
             recursive = True
+        elif opt in ("-v", "--verbose"):
+            verbose = True
         else:
             print("Wrong argument")
             sys.exit(2)
 
     print(inputdir)
-    crawl(inputdir, recursive)
+    crawler = Crawler(inputdir, recursive, verbose)
+    crawler.execute()
 
 
 if __name__ == "__main__":
