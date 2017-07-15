667820d823be03655488fd0cf82d5bf656770326
Stavros Georgousis <grgeosteve@gmail.com>
2017-07-15 15:24:09 +0300

    Add script for removing the duplicate files

---
+++ remove_duplicates.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import find_duplicates
+
+
+if __name__ == '__main__':
+    mypath = sys.argv[1]
+
+    dirs = find_duplicates.listDirectories(mypath)
+    if dirs == []:
+        return
+
+    rmlist_files = []
+    for d in dirs:
+        rmfile = d + ".removelist"
+        if ospath.isfile(rmfile):
+            rmlist_files.append(rmfile)
+
+
+    for rmfile in rmlist_files:
+        print(rmfile)
+        with open(rmfile, 'r') as f:
+            for line in f:
+                fname = line.split("\n")[0]
+                if find_duplicates.is_writable(fname):
+                    try:
+                        os.remove(fname)
+                    except KeyboardInterrupt:
+                        if os.path.isfile(fname):
+                            os.remove(fname)
+                        print("Exiting")
+                        return
+            
